2b8e95bbcb8de664caa93ff05602cd2bbf94bcea
Mishaal Rahman <mishalrm@gmail.com>
2026-06-05 15:06:27 +0400

    Added more styling on the sign in page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, Card, Col, Container, Form, Row } from "react-bootstrap";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 
 
 const LoginPage = () => {
@@ -15,35 +15,47 @@ const LoginPage = () => {
 
     return (
         <div className="">
-            <Container fluid className="">
-                <Row>
-                    <Col>
-                        <Card>
-                            <Card.Body>
-                                <h3>Sign in</h3>
-                                <Form onSubmit={handleSignIn}>
-                                    <Form.Group >
-                                        <Form.Control
-                                            type="text" placeholder="Username or email"
-                                            value={userName}
-                                            onChange={(e) => setUserName(e.target.value)}>
-                                        </Form.Control>
-                                    </Form.Group>
-                                    <Form.Control
-                                        type="password" placeholder="Password"
-                                        value={password}
-                                        onChange={(e) => setPassword(e.target.value)}>
-                                    </Form.Control>
-                                    <Form.Check type="checkbox" label="keep me signed in"
-                                        checked={persistSession}
-                                        onChange={(e) => setPersistSession(e.target.checked)}>
-                                    </Form.Check>
-                                    <Button type="submit">
-                                        Sign in
-                                    </Button>
-                                </Form>
-                            </Card.Body>
-                        </Card>
+            <Container fluid className="min-vh-100 d-flex align-items-center bg-white px-4">
+                <Row className="w-100 align-items-center justify-content-center">
+                    <Col xs={12} md={5} lg={4} className="mx-auto">
+
+                        <h3 className="m-2 fs-2">Sign in</h3>
+                        <Form onSubmit={handleSignIn}>
+                            <Form.Group >
+                                <Form.Control
+                                    type="text" placeholder="Username or email"
+                                    value={userName}
+                                    onChange={(e) => setUserName(e.target.value)}
+                                    className="p-2 m-2  border-dark border-2 rounded-0">
+                                </Form.Control>
+                            </Form.Group>
+                            <Form.Control
+                                type="password" placeholder="Password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                className="p-2 m-2 border-dark border-2 rounded-0"
+                            >
+                            </Form.Control>
+                            <div>
+                                <Form.Check type="checkbox" label="Keep me signed in"
+                                    checked={persistSession}
+                                    onChange={(e) => setPersistSession(e.target.checked)}
+                                    className="p-2 m-2 rounded-0 ms-4 custom-checkbox">
+                                </Form.Check>
+                            </div>
+                            <Button type="submit" className="w-100 rounded-0 p-2 ms-2"
+                                style={{ backgroundColor: '#3f3f46' }}
+                            >
+                                Sign in
+                            </Button>
+                        </Form>
+                        <div className="d-flex align-items-center my-4">
+                            <hr className="flex-grow-1 border-gray-300 my-0" />
+                            <span className="mx-3 text-muted small fw-medium text-nowrap" style={{ fontSize: '0.75rem' }}>
+                                Or Sign In With
+                            </span>
+                            <hr className="flex-grow-1 border-gray-300 my-0" />
+                        </div>
                     </Col>
                     <Col>
                         Column with the image
