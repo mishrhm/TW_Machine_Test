25c81bfa3f184ca5b31d42f0bd80331e5b036092
Mishaal Rahman <mishalrm@gmail.com>
2026-06-05 18:14:51 +0400

    Added dispatch to load more event to the loadMore component

--- src/components/loadMoreButton.tsx
+++ src/components/loadMoreButton.tsx
@@ -1,9 +1,14 @@
 import { Button } from "react-bootstrap";
+import { useAppDispatch } from "../app/hooks";
+import { loadMore } from "../features/countries/countriesSlice";
 
 export function LoadMoreButton() {
+    const dispatch = useAppDispatch();
+
     return (
         <div className="text-center my-5">
             <Button
+                onClick={() => { dispatch(loadMore()) }}
                 className="rounded-0 border-0 px-4 py-2 text-sm text-white font-medium"
                 style={{ backgroundColor: '#3f3f46' }}
                 onMouseEnter={(e) => (e.currentTarget.style.backgroundColor = '#27272a')}
