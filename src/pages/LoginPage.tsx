baa251bc907797ba7dc1ee26f0a97a4b73f91bfe
Mishaal Rahman <mishalrm@gmail.com>
2026-06-05 14:17:23 +0400

    Developing Login page ui

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,9 +1,24 @@
+import { Card, Col, Container, Row } from "react-bootstrap";
 
 
 const LoginPage = () => {
     return (
         <div>
-            Login Page
+            <div>
+                Login Page
+            </div>
+            <Container >
+                <Row>
+                    <Col>
+                        <Card>
+                            <Card.Body>
+                                <h3>Login</h3>
+                                <p>Credentials here</p>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
