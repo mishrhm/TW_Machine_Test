25fb09ccc358c6c76bf917f8471e6dd5c424e177
Mishaal Rahman <mishalrm@gmail.com>
2026-06-05 18:39:26 +0400

    When api failed flag was failing in slider, added failure fallback and tried to constraint the flag image in the slider

--- src/components/customSlider.tsx
+++ src/components/customSlider.tsx
@@ -2,12 +2,11 @@
 
 import React, { useState } from 'react';
 import { useAppSelector } from '../app/hooks';
+import { Spinner } from 'react-bootstrap';
 
 const CustomSlider: React.FC = () => {
 
-    const { list } = useAppSelector((state) => state.countries);
-
-
+    const { list, status } = useAppSelector((state) => state.countries);
 
     const [activeSlide, setActiveSlide] = useState(0);
     const totalSlides = 5;
@@ -15,12 +14,27 @@ const CustomSlider: React.FC = () => {
     const handlePrev = () => setActiveSlide((prev) => (prev === 0 ? totalSlides - 1 : prev - 1));
     const handleNext = () => setActiveSlide((prev) => (prev === totalSlides - 1 ? 0 : prev + 1));
 
+
+
+    if (status === 'loading' || list.length === 0) {
+        return (
+            <div className="position-relative w-100 h-100 border border-dark d-flex flex-column align-items-center justify-content-center bg-light min-h-[400px]">
+                <Spinner animation="border" variant="dark" size="sm" />
+                <span className="text-xs text-muted mt-2">Loading slider...</span>
+            </div>
+        );
+    }
+
+    const currentCountry = list[activeSlide] || list[0];
+
+
+
     return (
         <div className="position-relative w-100 h-100 border border-dark d-flex flex-column align-items-center justify-content-center bg-light min-h-[400px]">
-            <div className="text-center m-4">
-                <img src={list[activeSlide].flag}
-                    alt={`${list[activeSlide].name} flag`}
-                    className="w-100 h-100 object-fit-cover" />
+            <div className="text-center m-4 max-w-[320px] h-[200px]">
+                <img src={currentCountry.flag}
+                    alt={`${currentCountry.name} flag`}
+                    className="w-full h-full max-h-full object-fit-cover object-contain img-fluid transition-all duration-300" />
             </div>
 
             <div className="position-absolute bottom-0 mb-3 d-flex align-items-center gap-2 px-3 py-1 ">
