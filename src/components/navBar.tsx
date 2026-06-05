190459b3d6651b2fabe41a6efc9cff72e4e5ab6b
Mishaal Rahman <mishalrm@gmail.com>
2026-06-05 18:11:19 +0400

    Removed useState for active selection from navbar and added dispath selectedRegion selector from the store

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,19 +1,28 @@
-import { useState } from "react";
+
 import { Nav } from "react-bootstrap"
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import { setRegion } from "../features/countries/countriesSlice";
 
 export const Navbar = () => {
-    const [activeTab, setActiveTab] = useState('All');
+    const dispatch = useAppDispatch();
+
+    const selectedRegion = useAppSelector((state) => state.countries.selectedRegion);
+
 
     return (
         <header className="d-flex justify-content-between align-items-center mb-2 px-1">
             <h5 className="fw-bold m-0">Countries</h5>
-            <Nav variant="pills" activeKey={activeTab} onSelect={(k) => setActiveTab(k || 'All')}>
+            <Nav variant="pills" activeKey={selectedRegion} onSelect={(k) => {
+                if (k) {
+                    dispatch(setRegion(k));
+                }
+            }}>
                 {['All', 'Asia', 'Europe'].map((tab) => (
                     <Nav.Item key={tab}>
                         <Nav.Link
                             eventKey={tab}
                             className={`rounded-0 bg-transparent py-1 px-3 border-bottom border-2 
-                                ${activeTab === tab ? 'border-dark text-dark fw-bold' : 'border-light text-secondary '
+                                ${selectedRegion === tab ? 'border-dark text-dark fw-bold' : 'border-light text-secondary '
                                 }`}
                         >
                             {tab}
