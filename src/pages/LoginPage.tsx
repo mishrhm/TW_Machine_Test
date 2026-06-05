d6d30310d6e0bb4f91a411cb21a279b02449bcdb
Mishaal Rahman <mishalrm@gmail.com>
2026-06-05 15:41:37 +0400

    Added password validation and sign in logic front end

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,16 +1,40 @@
 import { useState } from "react";
-import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import { Alert, Button, Col, Container, Form, Row } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 
 const LoginPage = () => {
 
+    const navigate = useNavigate();
+
     const [userName, setUserName] = useState("");
     const [password, setPassword] = useState("");
     const [persistSession, setPersistSession] = useState(false);
+    const [error, setError] = useState<string | null>(null);
 
     function handleSignIn(e: React.SubmitEvent): void {
         e.preventDefault();
+        setError(null);
         console.log({ userName, password, persistSession })
+        const isPasswordValid = validatePassword(password);
+        if (!userName.trim() || !password) {
+            setError('Please enter your username and password');
+            return;
+        }
+        if (!isPasswordValid) {
+            setError('Password must be minimum 8 characters long  (consist of atleast 1 Capital letter, 1 number & 1 symbol)')
+            return;
+        }
+        navigate('/home');
+    }
+
+    function validatePassword(password: string): boolean {
+        //Password must be minimum 8 characters long  (consist of atleast 1 Capital letter, 1 number & 1 symbol)
+        const minLength = password.length >= 8;
+        const hasCapital = /[A-Z]/.test(password)
+        const hasNumber = /[0-9]/.test(password)
+        const hasSymbol = /[^A-Za-z0-9]/.test(password)
+        return minLength && hasCapital && hasNumber && hasSymbol;
     }
 
     return (
@@ -21,6 +45,11 @@ const LoginPage = () => {
 
                         <h3 className="m-2 fs-2">Sign in</h3>
                         <h5 className="m-2 py-2 fs-6">New User? <span className="fs-6 text-primary">Create an account</span></h5>
+                        {error && (
+                            <Alert variant="danger" className="rounded-0 py-2 small border-0 bg-red-50 text-red-700">
+                                {error}
+                            </Alert>
+                        )}
                         <Form onSubmit={handleSignIn}>
                             <Form.Group >
                                 <Form.Control
