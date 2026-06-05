94b1c476852293efd1cd25db000a8b4ff9b21bde
Mishaal Rahman <mishalrm@gmail.com>
2026-06-05 17:32:11 +0400

    Added shadow to maingrid

--- src/components/mainGrid.tsx
+++ src/components/mainGrid.tsx
@@ -22,16 +22,17 @@ export const MainGrid = () => {
         <Row className="g-3">
             {MOCK_COUNTRIES.map((country, idx) => (
                 <Col xs={12} md={6} key={idx}>
-                    <div className="border border-dark p-3 d-flex align-items-center bg-white shadow-sm transition-transform hover:translate-y-[-2px]">
+                    <div className="border border-dark p-2 d-flex align-items-center bg-white shadow-md transition-transform hover:translate-y-[-2px]"
+                        style={{ boxShadow: '4px 4px 0px 0px #cfcfcf', }}>
                         <div
                             className=" d-flex align-items-center justify-content-center me-3 border "
-                            style={{ width: '80px', height: '50px', flexShrink: 0 }}
+                            style={{ width: '100px', height: '80px', flexShrink: 0, }}
                         >
                             🖼️
                         </div>
                         <div>
-                            <h6 className="fw-bold  mb-0.5">{country.name}</h6>
-                            <p className="text-xs text-muted mb-0">{country.region}</p>
+                            <h6 className="mb-1">{country.name}</h6>
+                            <p className="fs-6 text-muted mb-0">{country.region}</p>
                         </div>
                     </div>
                 </Col>
